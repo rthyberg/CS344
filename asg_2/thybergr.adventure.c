ab90ee2f86773858bce2ade90fa8832ee7b5297c
rthyberg <robertthyberg@gmail.com>
2016-10-16 17:28:07 -0700

    added in typdef and enums

--- asg_2/thybergr.adventure.c
+++ asg_2/thybergr.adventure.c
@@ -1,5 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+typedef enum RoomType RoomType;
+typedef struct Room Room;
+
+// enum will add clarity to what type of room is being defined
+enum RoomType {START_ROOM, END_ROOM, MID_ROOM};
+
+struct Room {
+     char* name;             // room name
+     Room** connect;         // edge list of connections
+     RoomType* rtype;        // enum for what kind of room it is
+
+};
+int generateFiles();
+int createRooms();
 
 int main() {
+    srand();
     return 1;
+}
+
+int generateFiles() {
+    return 0;
 }
