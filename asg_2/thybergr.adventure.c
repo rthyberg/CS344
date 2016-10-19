25e02e87694c0a2509cc9e7c18a843efdc897e1f
Robert Thyberg <thybergr@eos-class.engr.oregonstate.edu>
2016-10-19 15:46:11 -0700

    moving back to local machine

--- asg_2/thybergr.adventure.c
+++ asg_2/thybergr.adventure.c
@@ -10,39 +10,128 @@
 typedef enum RoomType RoomType;
 typedef struct Room Room;
 extern int errno;
+
 // enum will add clarity to what type of room is being defined
 enum RoomType {START_ROOM, END_ROOM, MID_ROOM};
 
 struct Room {
      char* name;             // room name
-     Room* edge[7];         // edge list of connections
-     RoomType* rtype;        // enum for what kind of room it is
+     Room** edge;         // edge list of connections
+     RoomType rtype;        // enum for what kind of room it is
 
 };
-int generateFiles(int[][7]);
-int createConnections();
-Room* roomBuilder(FILE*);
+char**  generateFiles(int[][7]);
+char** createConnections();
+Room* buildRoom(char*);
+int freeRoom(Room* cell);
+void makeEdgesRoom(Room**, char**);
 FILE* myOpen(char*, char*, char*);
 void print_graph(int[][7]);
+void print_room(Room*);
 
 int main() {
     srand(time(0)); // seed the rand function with current time
-    createConnections();
+    char** files = createConnections();
 //    generateFiles(pid);
-    return 1;
+    int i;
+    Room* room_list[7];
+    for(i = 0; i<7; i++) {
+        room_list[i] = buildRoom(files[i]);
+    }
+    makeEdgesRoom(room_list, files);
+    for(i = 0; i < 7; i++) {
+        print_room(room_list[i]);
+    }
+        //printf("%s\n",files[0]);
+    for(i = 0; i <7; i++) {
+        freeRoom(room_list[i]);
+    }
+    /*
+        free(files[1]);
+        free(files[2]);
+        free(files[3]);
+        free(files[4]);
+        free(files[5]);
+        free(files[6]);
+        //free(files[7]);
+        */
+    return 0;
 }
-/*
+
+// open file and create edge list connections to other Rooms
+void makeEdgesRoom(Room** cells, char** filenames) {
+    int i;
+    for(i = 0; i < 7; i++) {
+        FILE* fp = myOpen(filenames[i], "r", "makeEdgesRoom"); // open file
+        char* buffer = NULL;
+        size_t len = 0;
+        int cflag = 0;
+        int count = 0;
+        while(getdelim(&buffer, &len, ':', fp) != -1) { // find the connections line
+           if(cflag == 1) {
+                char cpy[100];
+                strcpy(cpy, buffer);
+                char* token = strtok(cpy, "\n");
+                int j;
+                for(j=0; j<7; j++) { //find the cell pointer correlated to that connections
+                    if(!strstr(token, cells[j]->name)==0) {
+                       cells[i]->edge[count] = cells[j]; // push onto the array
+                       count++;
+                   }
+                }
+           }
+           if(strstr(buffer,"CONNECTION")) { //if found the next line will contain a connection
+                    cflag = 1;
+           }
+        }
+        fclose(fp);
+    }
+}
+
+// Builds a single room struct from string file name
 Room* buildRoom(char* input){
-   Room *new_room =  (Room*)malloc(sizeof Room);
-   for(i=0; i < 7; i++;) {
+   // allocate space for a room
+   Room *new_room = (Room*)malloc(sizeof(Room));
+   new_room->name = (char*)malloc(50*sizeof(char));
+   new_room->edge = (Room**)malloc(7*sizeof(Room*));
+   int i;
+   // set the array to null
+   for(i=0; i < 7; i++) {
       new_room->edge[i] = NULL;
    }
-   fp = myOpen(input, "r", "buildRoom");
-   char buffer[100]
-   while(fget 
+   FILE* fp = myOpen(input, "r", "buildRoom");
+   char* buffer = NULL;
+   size_t len = 0;
+   int nflag = 0;
+   int tflag = 0;
+   while(getdelim(&buffer, &len, ':', fp) != -1) {
+      if(nflag == 1) { // if name is found add it to the room
+           char cpy[100];
+           strcpy(cpy, buffer);
+           char* token = strtok(cpy, "\n");
+           strcpy(new_room->name, token);
+           nflag = 0;
+       } else if(tflag == 1) { // if type is found add it to the room
+           if(strstr(buffer, "START_ROOM")) {
+               new_room->rtype = START_ROOM;
+           } else if(strstr(buffer, "MID_ROOM")) {
+               new_room->rtype = MID_ROOM;
+           } else if(strstr(buffer, "END_ROOM")) {
+               new_room->rtype = END_ROOM;
+           }
+           tflag = 0;
+       } else if(strstr(buffer,"ROOM NAME")) { // checks to see if next line conatins a room name
+           nflag = 1;
+       } else if(strstr(buffer,"ROOM TYPE")) { // check to see if next line contains a type
+           tflag = 1;
+       }
+   }
+   free(buffer);
+   fclose(fp);
+   return new_room;
 }
-*/
-int generateFiles(int matrix[][7]) {
+
+char** generateFiles(int matrix[][7]) {
     // List of hardcoded rooms
     char id [50];
     int getPid = getpid(); // get the current process id
@@ -60,12 +149,15 @@ int generateFiles(int matrix[][7]) {
    int rooms_taken [7] = {-1,-1,-1,-1,-1,-1,-1};
    int taken = 0; // flag indicating if room has been used
    int count = -1;
-   char list_of_rooms[7][100];
+   char** list_of_rooms=(char**)malloc(7*sizeof(char*));
+   for(i=0; i <7; i++) {
+       list_of_rooms[i]=(char*)malloc(100*sizeof(char));
+   }
    // generate 6 rooms. count starts at -1
     while(count < 6) {
         int randomRoom = (int)(rand()%(10)+0);
         // check if taken
-        for(i=0; i< 7; i++) {
+        for(i = 0; i< 7; i++) {
             if(randomRoom == rooms_taken[i]) {
                 taken = 1;
             }
@@ -81,14 +173,14 @@ int generateFiles(int matrix[][7]) {
             fclose(current);
             strcpy(list_of_rooms[count], file_name); // keep a list of our path names for later use
         }
-        taken=0;
+        taken = 0;
     }
         int base_con;
         int num_of_con;
-        for(count=0; count < 7; count++) {
+        for(count = 0; count < 7; count++) {
             FILE* current = myOpen(list_of_rooms[count], "a", "generateFiles()");
-            num_of_con=0;
-            for(base_con=0; base_con < 7; base_con++) {
+            num_of_con = 0;
+            for(base_con = 0; base_con < 7; base_con++) {
                 if(matrix[count][base_con] == 1) {
                     num_of_con++;
                     fprintf(current, "CONNECTION %d: %s\n", // print the connections to the file
@@ -97,21 +189,21 @@ int generateFiles(int matrix[][7]) {
         // assign start and end to random which we declared earlier.
                 if(base_con == 6) { // if we are done adding connections add type
                     if(count == start) {
-                        fprintf(current, "ROOM TYPE:START_ROOM\n");
+                        fprintf(current, "ROOM TYPE: START_ROOM\n");
                     } else if(count == end) {
-                        fprintf(current, "ROOM TYPE:END_ROOM\n");
+                        fprintf(current, "ROOM TYPE: END_ROOM\n");
                     } else {
-                        fprintf(current, "ROOM TYPE:MID_ROOM\n");
+                        fprintf(current, "ROOM TYPE: MID_ROOM\n");
                     }
                 }
             }
             fclose(current);
         }
-    return 0;
+    return list_of_rooms;
 }
 
 // function that will semi-randomly generate connections to write to our files
-int createConnections() {
+char** createConnections() {
     // Current connections is odd 7*3=21. Must be even.
     // hard code one as 4 connections so we have 22 connections
     // we have 11 edges must maintain odd number of edges
@@ -130,8 +222,8 @@ int createConnections() {
 
 // random bonus connection
    while(maxConnections>0) {
-       int rc=rand()%(7-0);
-       int rc2=rand()%(7-0);
+       int rc = rand()%(7-0);
+       int rc2 = rand()%(7-0);
        if(connections[rc] < 7
          && connections[rc2] < 7
          && matrix[rc][rc2] == 0
@@ -145,8 +237,8 @@ int createConnections() {
        }
   }
 
-    generateFiles(matrix);
-    return 0;
+    char** result = generateFiles(matrix);
+    return result;
 }
 
 // abstract away file opening with error checking to make other errors more clear
@@ -163,7 +255,7 @@ FILE* myOpen(char* path, char* mode, char* function) {
 void print_graph(int graph [7][7]) {
     int i;
     int count=0;;
-    for(i=0; i < 7; i++) {
+    for(i = 0; i < 7; i++) {
         int j;
         printf("Node {%d}: ", i);
         for(j=0; j< 7; j++) {
@@ -176,3 +268,35 @@ void print_graph(int graph [7][7]) {
     }
         printf("# of Vertex: %d\n # of Edges: %d\n", count, (count/2));
 }
+
+// print room struct
+void print_room(Room* cell) {
+    printf("ROOM NAME: %s\n", cell->name);
+    int i = 0;
+    while(cell->edge[i] != NULL) {
+        printf("CONNECTION %d: %s\n", i, cell->edge[i]->name);
+        i++;
+    }
+    if((cell->rtype) == START_ROOM) {
+        printf("ROOM TYPE: %s\n","START_ROOM" );
+    } else if((cell->rtype) == MID_ROOM) {
+        printf("ROOM TYPE: %s\n","MID_ROOM" );
+    } else if((cell->rtype) == END_ROOM) {
+        printf("ROOM TYPE: %s\n","END_ROOM" );
+    }
+}
+// frees Room
+int freeRoom(Room* cell) {
+    int i;
+    while(cell->edge[i] != NULL) {
+        free(cell->edge[i]);
+        cell->edge[i]=NULL;
+        i++;
+    }
+    free(cell->name);
+    cell->name = NULL;
+    free(cell);
+    cell=NULL;
+    return 0;
+}
+
