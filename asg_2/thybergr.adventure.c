b32160f69e0eaea9f6b3503d21da7d1bd521754f
rthyberg <robertthyberg@gmail.com>
2016-10-19 18:33:17 -0700

    Game loop implemented, need to test on EOS

--- asg_2/thybergr.adventure.c
+++ asg_2/thybergr.adventure.c
@@ -25,6 +25,9 @@ char** createConnections();
 Room* buildRoom(char*);
 int freeRoom(Room* cell);
 void makeEdgesRoom(Room**, char**);
+void gameLoop(Room**);
+void printGameRoom(Room*);
+int getInputGame(char*, Room*);
 FILE* myOpen(char*, char*, char*);
 void print_graph(int[][7]);
 void print_room(Room*);
@@ -32,30 +35,93 @@ void print_room(Room*);
 int main() {
     srand(time(0)); // seed the rand function with current time
     char** files = createConnections();
-//    generateFiles(pid);
     int i;
     Room* room_list[7];
     for(i = 0; i<7; i++) {
         room_list[i] = buildRoom(files[i]);
     }
     makeEdgesRoom(room_list, files);
-    for(i = 0; i < 7; i++) {
-        print_room(room_list[i]);
-    }
-        //printf("%s\n",files[0]);
+    gameLoop(room_list);
     for(i = 0; i <7; i++) {
         freeRoom(room_list[i]);
+        free(files[i]);
     }
-    /*
-        free(files[1]);
-        free(files[2]);
-        free(files[3]);
-        free(files[4]);
-        free(files[5]);
-        free(files[6]);
-        //free(files[7]);
-        */
     return 0;
+}
+
+void gameLoop(Room** cells) {
+    int i = 0;
+    Room* current=NULL;
+    char path[100][100]; // really hope it doesnt take you a hundred tries
+    int path_count = 0;
+    // find the starting room before the game begins
+    int end_room = 0;
+    for(i = 0; i < 7; i++) {
+        if(cells[i]->rtype == START_ROOM) {
+            current = cells[i];
+        }
+        if(cells[i]->rtype == END_ROOM) {
+            end_room = 1;
+        }
+    }
+    if(end_room == 0) {
+        fprintf(stderr, "No end room was found");
+        exit(1);
+    }
+    while(current->rtype != END_ROOM) {
+        int valid_room = -1;
+        while(valid_room == -1) {
+            printGameRoom(current);
+            char buffer[100];
+            valid_room = getInputGame(buffer, current);
+            if(valid_room == -1) {
+                printf("\nHUH? I DON'T UNDERSTAND THAT ROOM, TRY AGAIN.\n\n");
+            } else {
+                current = current->edge[valid_room];
+                strcpy(path[path_count], current->name);
+                path_count++;
+                printf("\n");
+            }
+        }
+    }
+    printf("YOU HAVE FOUND THE END ROOM. CONGRATULATIONS!\n");
+    printf("YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", path_count);
+    for(i = 0; i < path_count + 1; i++) {
+        printf("%s\n", path[i]);
+    }
+}
+
+// print current rooms according to required format
+void printGameRoom(Room* cell) {
+    printf("CURRENT LOCATION: %s\n", cell->name);
+    printf("POSSIBLE CONNECTIONS: ");
+    int i = 0;
+    while(cell->edge[i] != NULL) {
+        if(cell->edge[i+1] == NULL) { // if last element use a .
+            printf("%s.\n",cell->edge[i]->name);
+        } else {
+            printf("%s, ", cell->edge[i]->name); // print the list
+        }
+        i++;
+    }
+    printf("WHERE TO? >");
+ }
+
+// abstract gettin user input to give more clarity to game loop
+// return value will be where in the edgelist the input is or -1
+int getInputGame(char* buffer, Room* cell) {
+    int valid = -1; // return value whether the input is in the cell edgelist
+    int i = 0;
+    fgets(buffer, 100, stdin); // get input form user
+    buffer[strcspn(buffer, "\n")] = 0; //set the \n char to null byte;
+    while(cell->edge[i] != NULL) { // go through edgelist comparing buffer
+        if(strcmp(buffer, cell->edge[i]->name) == 0) {
+            valid = i;
+            break; // if found we can break early
+        }
+        i++;
+    }
+    return valid;
 }
 
 // open file and create edge list connections to other Rooms
@@ -74,7 +140,7 @@ void makeEdgesRoom(Room** cells, char** filenames) {
                 char* token = strtok(cpy, "\n");
                 int j;
                 for(j=0; j<7; j++) { //find the cell pointer correlated to that connections
-                    if(!strstr(token, cells[j]->name)==0) {
+                    if(!(strstr(token, cells[j]->name)==0)) {
                        cells[i]->edge[count] = cells[j]; // push onto the array
                        count++;
                    }
@@ -144,8 +210,12 @@ char** generateFiles(int matrix[][7]) {
     mkdir(dir_name, S_IRWXU | S_IRWXG | S_IRWXO);
    // initialise vars
    int i;
-   int start = rand()%(7-0);
-   int end = rand()%(7-0);
+   int start = 0;
+   int end = 0;
+   while(start == end) { // avoid case where start and end are the same
+       start = rand()%(7-0);
+       end = rand()%(7-0);
+   }
    int rooms_taken [7] = {-1,-1,-1,-1,-1,-1,-1};
    int taken = 0; // flag indicating if room has been used
    int count = -1;
@@ -287,12 +357,13 @@ void print_room(Room* cell) {
 }
 // frees Room
 int freeRoom(Room* cell) {
-    int i;
+    int i=0;
+    // Since we are gonna free all nodes we dont free the pointers in the array here
     while(cell->edge[i] != NULL) {
-        free(cell->edge[i]);
         cell->edge[i]=NULL;
         i++;
     }
+    free(cell->edge);
     free(cell->name);
     cell->name = NULL;
     free(cell);
